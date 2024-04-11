33a42af53c7241bcf337f2d9fcef584458d9b55d
LauraCantalicio <20211845@itla.edu.do>
2024-04-11 13:40:21 -0300

    first commit

--- login/index.php
+++ login/index.php
@@ -82,7 +82,22 @@ include ('../app/config/conexion.php');
                     <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
                 </a>
             </div>-->
-
+            <!--<p class="mb-1">
+                <a href="forgot-password.html">I forgot my password</a>
+            </p>
+            <p class="mb-0">
+                <a href="register.html" class="text-center">Register a new membership</a>
+            </p>-->            <!--<p class="mb-1">
+                <a href="forgot-password.html">I forgot my password</a>
+            </p>
+            <p class="mb-0">
+                <a href="register.html" class="text-center">Register a new membership</a>
+            </p>-->            <!--<p class="mb-1">
+                <a href="forgot-password.html">I forgot my password</a>
+            </p>
+            <p class="mb-0">
+                <a href="register.html" class="text-center">Register a new membership</a>
+            </p>-->
 
             <!--<p class="mb-1">
                 <a href="forgot-password.html">I forgot my password</a>
